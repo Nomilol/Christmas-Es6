728bbeafb785d76a08c8306389f3cb1497a099b5
Axel <lezinaxel@gmail.com>
2016-12-20 14:53:03 +0100

    Il y a bien un ajout de la liste mais sans rien dedans pour le moment

--- public/app.js
+++ public/app.js
@@ -1,10 +1,16 @@
 submit.addEventListener('click', event => {
 	const input = document.getElementById('userName').value;
 	const span = document.getElementById('name');
+	const arrayName = [];
+	console.log(arrayName);
+
 	if (input === "") {
 		span.innerHTML = 'Père Noël';
 	} else {
 		span.innerHTML = input;
 		console.log(input);
 	}
+
+	const ul = document.createElement('li');
+	document.body.appendChild(ul);
 });
